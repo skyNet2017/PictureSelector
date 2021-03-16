ea2633d0b61c5bccc28c34e69456dfa513803009
hufy@akulaku.com <hufy@akulaku.com>
2021-03-16 17:09:18 +0800

    设置视频播放页状态栏底色；更新icon

--- localvideoplayer/src/main/java/com/hss01248/media/localvideoplayer/PictureVideoPlayByGSYActivity.java
+++ localvideoplayer/src/main/java/com/hss01248/media/localvideoplayer/PictureVideoPlayByGSYActivity.java
@@ -1,10 +1,13 @@
 package com.hss01248.media.localvideoplayer;
 
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.shuyu.gsyvideoplayer.GSYBaseActivityDetail;
@@ -13,6 +16,9 @@ import com.shuyu.gsyvideoplayer.listener.GSYStateUiListener;
 import com.shuyu.gsyvideoplayer.player.PlayerFactory;
 import com.shuyu.gsyvideoplayer.player.SystemPlayerManager;
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import static com.shuyu.gsyvideoplayer.video.base.GSYVideoView.CURRENT_STATE_AUTO_COMPLETE;
 
@@ -32,6 +38,9 @@ public class PictureVideoPlayByGSYActivity extends GSYBaseActivityDetail<Standar
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         PlayerFactory.setPlayManager(SystemPlayerManager.class);
+
+        setImmersiveStatusBar(false, getResources().getColor(R.color.black));
+
 
         videoPath = getIntent().getStringExtra(PATH);
         dismissPageWhenFinishPlay = getIntent().getBooleanExtra(TAG_DISMISSPAGEWHENFINISHPLAY,false);
@@ -194,4 +203,98 @@ public class PictureVideoPlayByGSYActivity extends GSYBaseActivityDetail<Standar
     public void onBackPressed() {
         super.onBackPressed();
     }
+
+
+    /**
+     * 设置状态栏透明
+     */
+    public void setTranslucentStatus(int statusBarPlaceColor) {
+
+        // 5.0以上系统状态栏透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(statusBarPlaceColor);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+    /**
+     * 设置沉浸式状态栏
+     *
+     * @param fontIconDark 状态栏字体和图标颜色是否为深色
+     */
+    public void setImmersiveStatusBar(boolean fontIconDark, int statusBarPlaceColor) {
+        setTranslucentStatus(statusBarPlaceColor);
+        if (fontIconDark) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                setStatusBarFontIconDark(true);
+            }
+        }
+    }
+
+    /**
+     * 设置Android状态栏的字体颜色，状态栏为亮色的时候字体和图标是黑色，状态栏为暗色的时候字体和图标为白色
+     *
+     * @param dark 状态栏字体是否为深色
+     */
+    public void setStatusBarFontIconDark(boolean dark) {
+        // 小米MIUI
+        try {
+            Window window = getWindow();
+            Class clazz = getWindow().getClass();
+            Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            int darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            if (dark) {    //状态栏亮色且黑色字体
+                extraFlagField.invoke(window, darkModeFlag, darkModeFlag);
+            } else {       //清除黑色字体
+                extraFlagField.invoke(window, 0, darkModeFlag);
+            }
+        } catch (Exception e) {
+            //ExceptionReporterHelper.reportException(e);
+        }
+
+        // 魅族FlymeUI
+        try {
+            Window window = getWindow();
+            WindowManager.LayoutParams lp = window.getAttributes();
+            Field darkFlag = WindowManager.LayoutParams.class
+                    .getDeclaredField("MEIZU_FLAG_DARK_STATUS_BAR_ICON");
+            Field meizuFlags = WindowManager.LayoutParams.class.getDeclaredField("meizuFlags");
+            darkFlag.setAccessible(true);
+            meizuFlags.setAccessible(true);
+            int bit = darkFlag.getInt(null);
+            int value = meizuFlags.getInt(lp);
+            if (dark) {
+                value |= bit;
+            } else {
+                value &= ~bit;
+            }
+            meizuFlags.setInt(lp, value);
+            window.setAttributes(lp);
+        } catch (Exception e) {
+            //ExceptionReporterHelper.reportException(e);
+        }
+        // android6.0+系统
+        // 这个设置和在xml的style文件中用这个<item name="android:windowLightStatusBar">true</item>属性是一样的
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decor = getWindow().getDecorView();
+            int ui = decor.getSystemUiVisibility();
+            if (dark) {
+                //设置状态栏中字体的颜色为黑色
+                ui |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            } else {
+                //设置状态栏中字体颜色为白色
+                ui &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+            decor.setSystemUiVisibility(ui);
+        }
+    }
+
 }
